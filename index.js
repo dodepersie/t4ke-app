8d56e0a95b56ac81a48ab297b0a32e123d89af61
I Dewa Gede Mahadi Saputra <dodepersie@gmail.com>
2025-07-07 09:24:51 +0800

    fix: index.js again

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const app = express();
 const tiktokApiRouter = require("./api/tiktok.js");
 
 app.get("/", (req, res) => {
-  res.status(404).send("Cannot GET /");
+  res.status(404).json({
+    status: "error",
+    message: "Cannot GET /",
+  });
 });
 
 app.use("/api/tt", tiktokApiRouter);
